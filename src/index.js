de5313b522c6b9943d9a0d67f6e8fc3ce853e9be
viraj-2393 <viraj.sased@gmail.com>
2022-07-17 08:09:17 +0530

    piechart added and some layout changes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import NavMenu  from './components/navmenu.js';
 import ExpenseChart from './components/expense_chart.js';
+import ExpensePieChart from './components/expense_pie_chart.js';
 import './components/css/rightbody.css';
 function RightBody(){
   return (
     <>
       <div className="right_body">
           <NavMenu/>
-          <ExpenseChart/>
+          <div className='charts'>
+            <div className='chart1'><ExpenseChart/></div>
+            <div className='chart2'><ExpensePieChart/></div>
+          </div>
+          
       </div>
     </>
   );
