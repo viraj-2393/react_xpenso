0149bc3578943f43f3d15e1412458177f912b8c9
viraj-2393 <viraj.sased@gmail.com>
2022-07-17 10:43:57 +0530

    budget card added

--- src/index.js
+++ src/index.js
@@ -25,6 +25,52 @@ function SideBarMenuItem(props){
   );
 }
 
+function BudgetAndTransactionStrip(props){
+  return (
+    <>
+     <div className='budget_card'>
+             <div className='budget_card_left'>
+              <div className='budget_card_icon_container'>
+                <span class="material-icons">{props.icon}</span>
+              </div>
+              <div className='budget_data'>
+                  <h4>{props.title}</h4>
+              </div>
+             </div>
+             <div className='budget_card_right'>
+              <h1>${props.amount}</h1>
+             </div>
+             
+      </div>
+    </>
+  );
+}
+
+function BudgetAndTransaction(props){
+  return (
+    <>
+      <div className="budgetandtransaction">
+        <div className='budget'>
+           <div className="budget_header">
+            <h3>Goal Budget</h3>
+            <span class="material-icons">hdr_strong</span>
+           </div>
+           <BudgetAndTransactionStrip icon='airplane_ticket' title='Travel' amount='500'/>
+           <BudgetAndTransactionStrip icon='school' title='Education' amount='400'/>
+           <BudgetAndTransactionStrip icon='movie' title='Entertainment' amount='199'/>
+           <BudgetAndTransactionStrip icon='ramen_dining' title='Grocery' amount='1500'/>
+           <BudgetAndTransactionStrip icon='assignment' title='Others' amount='2000'/>
+           
+        </div>
+        <div className='transaction'>
+
+        </div>
+             
+      </div>
+    </>
+  );
+}
+
 function RightBody(){
   return (
     <>
@@ -41,10 +87,14 @@ function RightBody(){
         </div>
         <div className="right_body">
           <NavMenu/>
+
           <div className='charts'>
             <div className='chart1'><ExpenseChart/></div>
             <div className='chart2'><ExpensePieChart/></div>
           </div>
+
+          <BudgetAndTransaction />
+
           
       </div>
       </div>
