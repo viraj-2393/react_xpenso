634718fb3609f07dc14a422dbf1f79511826be69
viraj-2393 <viraj.sased@gmail.com>
2022-07-20 09:00:06 +0530

    components restructuring and dynamic chart data added

--- src/components/expense_chart.js
+++ src/components/expense_chart.js
@@ -4,8 +4,10 @@ var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
  
 class ExpenseChart extends Component {	
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
+		this.pdp1 = props.chartDataPoint1;
+		this.pdp2 = props.chartDataPoint2;
 		this.toggleDataSeries = this.toggleDataSeries.bind(this);
 	}
 	
@@ -30,17 +32,17 @@ class ExpenseChart extends Component {
 				text: "Click Legend to Hide or Unhide Data Series"
 			}],
 			axisX: {
-				title: "States"
+				title: "Expenditure"
 			},
 			axisY: {
-				title: "Units Sold",
+				title: "Money Spent $",
 				titleFontColor: "#6D78AD",
 				lineColor: "red",
 				labelFontColor: "#6D78AD",
 				tickColor: "#3547AC"
 			},
 			axisY2: {
-				title: "Profit in USD",
+				title: "Money Added $",
 				titleFontColor: "#51CDA0",
 				lineColor: "#F0582E",
 				labelFontColor: "#51CDA0",
@@ -55,46 +57,20 @@ class ExpenseChart extends Component {
 			},
 			data: [{
 				type: "spline",
-				name: "Units Sold",
+				name: "Money Spent $",
 				showInLegend: true,
 				xValueFormatString: "MMM YYYY",
-				yValueFormatString: "#,##0 Units",
-				dataPoints: [
-					{ x: new Date(2017, 0, 1), y: 120 },
-					{ x: new Date(2017, 1, 1), y: 135 },
-					{ x: new Date(2017, 2, 1), y: 144 },
-					{ x: new Date(2017, 3, 1), y: 103 },
-					{ x: new Date(2017, 4, 1), y: 93 },
-					{ x: new Date(2017, 5, 1), y: 129 },
-					{ x: new Date(2017, 6, 1), y: 143 },
-					{ x: new Date(2017, 7, 1), y: 156 },
-					{ x: new Date(2017, 8, 1), y: 122 },
-					{ x: new Date(2017, 9, 1), y: 106 },
-					{ x: new Date(2017, 10, 1), y: 137 },
-					{ x: new Date(2017, 11, 1), y: 142 }
-				]
+				yValueFormatString: "$#,##0.#",
+				dataPoints: this.pdp1
 			},
 			{
 				type: "spline",
-				name: "Profit",
+				name: "Money Added $",
 				axisYType: "secondary",
 				showInLegend: true,
 				xValueFormatString: "MMM YYYY",
 				yValueFormatString: "$#,##0.#",
-				dataPoints: [
-					{ x: new Date(2017, 0, 1), y: 19034.5 },
-					{ x: new Date(2017, 1, 1), y: 0 },
-					{ x: new Date(2017, 2, 1), y: 27342 },
-					{ x: new Date(2017, 3, 1), y: 20088 },
-					{ x: new Date(2017, 4, 1), y: 20234 },
-					{ x: new Date(2017, 5, 1), y: 29034 },
-					{ x: new Date(2017, 6, 1), y: 30487 },
-					{ x: new Date(2017, 7, 1), y: 32523 },
-					{ x: new Date(2017, 8, 1), y: 20234 },
-					{ x: new Date(2017, 9, 1), y: 27234 },
-					{ x: new Date(2017, 10, 1), y: 33548 },
-					{ x: new Date(2017, 11, 1), y: 32534 }
-				]
+				dataPoints: this.pdp2
 			}]
 		}
 		
---
+++ src/components/transaction_form.js
@@ -0,0 +1,63 @@
+import './css/transaction.css';
+import { useState } from 'react';
+
+// function addTransaction(){
+//     let copy = [...txData];
+//     copy.push({id:"5", title: 'Others', date: Date().toString(), amount: '60', isVisible: true});
+//     setTxData(copy);
+
+//   }
+
+
+function TransactionForm (props){
+
+    const closeWindow = () => {
+       props.setVisibility(!props.isVisible);
+    }
+
+    const addTransaction = () => {
+          let copy = [...props.txData];
+          copy.push({id:copy.length+1, title: title, date: date, amount: amount, isVisible: true});
+          props.setTxData(copy);
+    }
+
+    let [title,setTitle] = useState('');
+    let [amount,setAmount] = useState('');
+    let date = Date().toString();
+
+    return (
+     <>
+       <div className='add_transaction_form'>
+
+        <div className='add_transaction_header'>
+          <h3> + Add Transaction</h3>
+          <button type='button' onClick={closeWindow}>X</button>
+        </div>
+        <div className='add_transaction_body'>
+           <label>
+            Enter category:<br/>
+            <select value={title} onChange={(e) =>  setTitle(e.target.value)}>
+              <option value='Entertainment'>Entertainment</option>
+              <option value='Travel'>Travel</option>
+              <option value='Education'>Education</option>
+              <option value='Health'>Health & Medicines</option>
+              <option value='Grocery'>Grocery</option>
+              <option value='Others'>Others</option>
+            </select>
+           </label>
+
+           <label>
+            Enter Amount:<br/>
+            <input type='text'  onChange={(e) =>  setAmount(e.target.value)}/>
+           </label>
+
+           <input type='submit' onClick={addTransaction}/>
+        </div>
+
+
+       </div>
+     </>
+    );
+ }
+
+ export default TransactionForm;
